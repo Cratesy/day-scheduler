88c8b51564b8774b5737e7cf0ddd9866412287f4
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-03-31 20:28:44 +0100

    added planner event and data types

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,20 @@ const renderCalendarEvents = () => {
   const plannerEvents = localStorage.getItem("plannerEvents");
 
   if (plannerEvents !== null) {
-    console.log(plannerEvents);
+    const currentHour = moment().hour();
+    // const currentHour = 11;
+    const timeBlocks = $(".container .row");
+    const callback = function () {
+      const timeBlockTime = Number.parseInt($(this).data("time"), 10);
+      if (timeBlockTime === currentHour) {
+        $(this).find("textarea").removeClass("past").addClass("present");
+      }
+      if (timeBlockTime > currentHour) {
+        $(this).find("textarea").removeClass("past").addClass("future");
+      }
+    };
+
+    timeBlocks.each(callback);
   } else {
     localStorage.setItem("plannerEvents", JSON.stringify({}));
   }
