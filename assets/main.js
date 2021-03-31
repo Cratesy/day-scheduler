779af191bb0d22ee963af0ccd8095821184ab77d
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-03-31 19:11:24 +0100

    added render for calender events

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,22 @@ const renderCurrentDate = () => {
   dateTime.text(displayNow);
 };
 
+const renderCalendarEvents = () => {
+  //get from local
+  const plannerEvents = localStorage.getItem("plannerEvents");
+
+  if (plannerEvents !== null) {
+    console.log(plannerEvents);
+  } else {
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
+  }
+};
+
 const onReady = () => {
+  // get current date
   renderCurrentDate();
+  // check for events
+  renderCalendarEvents();
 };
 
 $(document).ready(onReady);
